ea4f87f49997eab97b8fcb60c630617dccecdab4
Mumuni Tahiru <96925713+itspep@users.noreply.github.com>
2025-06-02 23:59:45 +0000

    setup webpack

---
+++ config/webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',  // Handles images
+        generator: {
+          filename: 'assets/images/[hash][ext][query]',
+        },
+      },
+    ],
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log("Webpack is working!");
+import logo from './assets/images/logo.svg';
+const img = document.createElement('img');
+img.src = logo;
+document.body.appendChild(img);
